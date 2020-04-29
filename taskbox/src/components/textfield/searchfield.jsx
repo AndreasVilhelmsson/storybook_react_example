186516f41d58c5b7ae27d0e7f2873c7c04c4c12b
andreas vilhelmsson <andreas.vilhelmsson@purplescout.se>
2020-04-29 09:49:36 +0200

    first commit

---
+++ taskbox/src/components/textfield/searchfield.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const searchfield = () => {
+    const [state , setstate] = useState(initialState)
+
+    onsChange = () => {
+        const text = event.target.value
+        state = setState(text);
+
+    }
+    return (
+        
+           
+                <input type="text" className="type"/>
+                <label>=""></label>
+            
+       
+    )
+}
